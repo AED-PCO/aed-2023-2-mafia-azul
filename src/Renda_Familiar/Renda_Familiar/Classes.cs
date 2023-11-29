3378f683d26a37b2c8d16984d0aca2ccce0165b9
Matheus Henrique Marques Canuto <matheushenriquecanuto77@gmail.com>
2023-11-28 22:14:27 -0300

    Populando os jsons para os testes

---
+++ src/Renda_Familiar/Renda_Familiar/Classes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renda_Familiar
+{
+    public class Pessoa
+    {
+        public string ID { get; set; }
+        public string login;
+        public string senha;
+        public string usuario;
+        public string Nome;
+        public string Data_de_nascimento;
+        public float Saldo;
+        public void debito(float valor)
+        {
+            this.Saldo = this.Saldo - valor;
+        }
+        public void credito(float valor)
+        {
+            this.Saldo = this.Saldo + valor;
+        }
+    }
+    public class Transacao
+    {
+        public string nome;
+        public string data;
+        public string valor;
+        public string idUsuario;
+
+    }
+}
